c0f662c295fff9b8a01e4f311e2df2cda5c5044e
Kayode Okunlade <okunladekayode@gmail.com>
2019-01-23 12:02:54 +0100

    chore(serverside): setup serverside framework

---
+++ app.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import morganLogger from 'morgan';
+import cors from 'cors';
+
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(morganLogger('combined'));
+
+app.get('/', (req, res) => {
+  res.status(200).json({
+    status: 200,
+    data: [],
+    message: 'Welcome to Politico Application'
+  });
+});
+
+app.use((req, res) => {
+  res.status(404).json({
+    status: 404,
+    error: 'Resource Not Found'
+  });
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`Server running on port: ${app.get('port')} ....`);
+});
+
+export default app;
