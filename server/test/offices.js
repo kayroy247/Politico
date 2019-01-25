365e00cb2ffc4b3ee3c952e13723fdacb77163ef
Kayode Okunlade <okunladekayode@gmail.com>
2019-01-25 15:20:35 +0100

    chore(unit test): Add unit test
    -Add test for all endpoints
    -Tests failed first in line with Test Driven Development
    
    Deliver #163408396

---
+++ server/test/offices.js
@@ -0,0 +1,88 @@
+import { describe, it } from 'mocha';
+import { use, request, expect } from 'chai';
+import chaiHttp from 'chai-http';
+import app from '../../app';
+
+use(chaiHttp);
+const httpRequest = request(app).keepOpen();
+
+describe('GET /', () => {
+  it('Should return the message of the base url', (done) => {
+    httpRequest
+      .get('/')
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body).to.be.an('object');
+        expect(res.body).to.have.property('message')
+          .to.be.equal('Welcome to Politico Application');
+        expect(res.body).to.have.property('data');
+        expect(res.body).to.have.property('status');
+      });
+    done();
+  });
+});
+
+describe('GET /api/v3', () => {
+  it('Should return version 1 welcome message', (done) => {
+    httpRequest
+      .get('/post/')
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body).to.have.property('message')
+          .to.be.equal('Welcome to Politico API VERSION 1');
+        expect(res.body).not.to.be.have.property('status');
+      });
+    done();
+  });
+});
+
+describe('GET /api/v1/offices', () => {
+  it('Should return all offices', (done) => {
+    httpRequest
+      .get('/api/v1/offices')
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body).to.have.property('message');
+        expect(res.body).to.have.property('data');
+        expect(res.body).to.be.an('object');
+      });
+    done();
+  });
+});
+
+describe('GET /api/v1/offices/1', () => {
+  it('Should fetch One office by officeID', (done) => {
+    httpRequest
+      .get('/api/v1/offices/1')
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body).to.have.property('message');
+        expect(res.body).to.have.property('data');
+        expect(res.body).to.be.an('object');
+      });
+    done();
+  });
+});
+
+
+describe('POST /api/v1/offices', () => {
+  it('Should create a new Political Office', (done) => {
+    const data = {
+      type: 'Presidential',
+      name: 'Presidential',
+      location: 'Nigeria'
+    };
+    httpRequest
+      .post('/api/v1/offices')
+      .send(data)
+      .end((err, res) => {
+        expect(res).to.have.status(201);
+        expect(res.body).to.have.property('message')
+          .to.be.equal('Political Office Successfully Created');
+        expect(res.body).to.be.an('object');
+        expect(res.body).to.have.property('data');
+        expect(res.body).to.have.property('status');
+      });
+    done();
+  });
+});
---
+++ server/test/parties.js
@@ -0,0 +1,83 @@
+import { describe, it } from 'mocha';
+import { use, request, expect } from 'chai';
+import chaiHttp from 'chai-http';
+import app from '../../app';
+
+use(chaiHttp);
+const httpRequest = request(app).keepOpen();
+
+describe('GET /api/v1/parties', () => {
+  it('Should return all parties', (done) => {
+    httpRequest
+      .get('/api/v1/parties')
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body).to.have.property('message');
+        expect(res.body).to.have.property('data');
+        expect(res.body).to.be.an('object');
+      });
+    done();
+  });
+});
+
+describe('GET /api/v1/parties/1', () => {
+  it('Should fetch One political party', (done) => {
+    httpRequest
+      .get('/api/v1/parties/1')
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body).to.have.property('message');
+        expect(res.body).to.have.property('data');
+        expect(res.body).to.be.an('object');
+      });
+    done();
+  });
+});
+
+describe('PATCH /api/v1/parcels/1/name', () => {
+  it('Should return error message is empty', (done) => {
+    httpRequest
+      .patch('/api/v1/parties/1/Action Alliance')
+      .end((err, res) => {
+        expect(res).to.have.status(400);
+        expect(res.body).to.have.property('error');
+        expect(res.body).to.be.an('object');
+      });
+    done();
+  });
+});
+
+describe('POST /api/v1/parties', () => {
+  it('Should Create a political party', (done) => {
+    const data = {
+      name: 'Action Alliance',
+      acronym: 'AA'
+    };
+    httpRequest
+      .post('/api/v1/parties')
+      .send(data)
+      .end((err, res) => {
+        expect(res).to.have.status(201);
+        expect(res.body).to.be.an('object');
+        expect(res.body).to.have.property('data');
+        expect(res.body).to.have.property('status');
+      });
+    done();
+  });
+});
+
+describe('DELETE /api/v1/parties/2', () => {
+  it('Should delete the specific parcel delivery order by ID', (done) => {
+    httpRequest
+      .delete('/api/v1/parties/2')
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body).to.have.property('message')
+          .to.be.equal('Political Party Successfully Deleted');
+        expect(res.body).to.be.an('object');
+        expect(res.body).to.have.property('data');
+        expect(res.body).to.have.property('status');
+      });
+    done();
+  });
+});
