e687c85a286699a238bfca7049c0227eeea01855
Kayode Okunlade <okunladekayode@gmail.com>
2019-02-02 15:52:49 +0100

    fix(travis): fix travis issue

--- server/test/parties.js
+++ server/test/parties.js
@@ -21,11 +21,32 @@ describe('Test offices endpoints', () => {
     done();
   });
 
+  it('Should Create a user account', async () => {
+    const data = {
+      firstname: 'Olusegun',
+      lastname: 'Samson',
+      email: 'samsam@gmail.com',
+      phoneNumber: '090847823733',
+      password: 'password',
+      passportURL: 'http://chapa.com',
+      isAdmin: true
+
+    }
+    const res = await request(app)
+      .post('/api/v1/auth/signup')
+      .set('Authorization', 'here too')
+      .send(data);
+    newToken = res.body.token;
+    expect(res).to.have.status(201);
+    expect(res.body).to.have.property('token');
+    expect(res.body).to.have.property('data');
+    expect(res.body).to.have.property('status');
+  });
   it('Should Login to get a token', async () => {
     const res = await request(app)
       .post('/api/v1/auth/login')
       .set('Authorization', 'here too')
-      .send({ email: 'okunladekayode@gmail', password: 'password' });
+      .send({ email: 'samsam@gmail.com', password: 'password' });
     newToken = res.body.token;
     expect(res).to.have.status(200);
     expect(res.body).to.have.property('token');
